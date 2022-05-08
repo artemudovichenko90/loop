f982b37edebe91127ce374019f113fdf986eaeb0
Artem Udovichenko <artemudovichenko90@gmail.com>
2022-05-08 20:24:31 +0300

    Create constants for game

---
+++ assets/js/constants.js
@@ -0,0 +1,7 @@
+const SECRET_NUMBER = 77;
+const HIGHER_NUMBER = "My number is higher, try again!";
+const LESS_NUMBER = "My number is less, try again!";
+const WELCOME = "I came up with a random number, try to guess!";
+const USER_INPUT = "Enter your number!";
+const VICTORY = "Congratulations, you guessed the number!"
+const COUNT_ATTEPMT = "The number of your attempts is: ";
